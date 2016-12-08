e66e5d336c14ab62755907619f3c9c4a623a7ff0
Jason Hee <jasonheecs@gmail.com>
2016-12-08 15:00:11 +0800

    Enabling site bash script now checks the nginx config after symlink

--- enable-site.sh
+++ enable-site.sh
@@ -4,3 +4,4 @@ set -e
 
 read -rp "Enter the filename (in sites-available) of the site to enable:" filename
 sudo ln -s "/etc/nginx/sites-available/${filename}" "/etc/nginx/sites-enabled/${filename}"
+sudo nginx -t

6c50c9219afe17d17dd224429e67945bbeec9423
Jason Hee <jasonheecs@gmail.com>
2016-12-08 12:39:30 +0800

    Added bash script to symlink site into sites-enabled

---
+++ enable-site.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+read -rp "Enter the filename (in sites-available) of the site to enable:" filename
+sudo ln -s "/etc/nginx/sites-available/${filename}" "/etc/nginx/sites-enabled/${filename}"
